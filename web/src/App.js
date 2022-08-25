605848cbb8db084e289399e71b465cbf5eec6dd0
Gary Anikin <garyanikin@users.noreply.github.com>
2022-08-26 00:51:27 +0300

    switch render to p5js

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import Sketch from "react-p5";
+
 import { clamp } from "lodash";
 import React, { useEffect } from "react";
 import photoSrc from "./photo.jpg";
-import polygonSrc from "./polygon.svg";
 import { useSpring, animated } from "@react-spring/web";
 
 // TODO use Tree shake
@@ -182,17 +183,15 @@ function App() {
       <div className="photo_overlay" />
       <div className="mini">
         <div className="minipreview">
-          <div className="preview" style={setMiniPreviewScale(45)}>
-            <div className="image">
-              <div className="image-container">
-                <animated.div
-                  style={animatedStyles}
-                  className="image-content"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="overlay" />
+          <Preview
+            x={style.x}
+            y={style.y}
+            scale={style.scale}
+            imageSize={imageSize}
+            image_src={image_src}
+            resizeTo={45}
+            key="mini"
+          />
         </div>
         <div className="profile">
           <div className="name">Маргарита Пшеничнова</div>
@@ -200,22 +199,158 @@ function App() {
         </div>
       </div>
       <div className="preview">
-        <div className="image">
-          <div className="image-container">
-            <animated.div style={animatedStyles} className="image-content" />
-          </div>
-        </div>
-        <div className="overlay" />
-        <Preview x={style.x} y={style.y} />
+        <Preview
+          x={style.x}
+          y={style.y}
+          scale={style.scale}
+          imageSize={imageSize}
+          image_src={image_src}
+          key="preview"
+          resizeTo={null}
+        />
       </div>
     </div>
   );
 }
 
-const Preview = animated(({ x, y }) => {
-  console.log(x, y);
-  return null;
-});
+let photoImage;
+const Preview = animated(
+  ({ x, y, image_src, imageSize, scale, key, resizeTo = null }) => {
+    const _size = resizeTo || getPolygonSize();
+
+    const bounds = getBounds(() => imageSize, { get: () => scale })();
+    const isRubber =
+      x < bounds.left ||
+      x > bounds.right ||
+      y < bounds.top ||
+      y > bounds.bottom;
+    const size = getPolygonSize();
+    const setup = (p5, canvasParentRef) => {
+      console.log("SETUP P5JS RENDERER CANVAS!!!!");
+      // use parent to render the canvas in this ref
+      // (without that p5 will render the canvas outside of your component)
+      p5.createCanvas(_size, _size).parent(canvasParentRef);
+    };
+
+    const preload = (p5) => {
+      photoImage = p5.loadImage(image_src);
+    };
+
+    const draw = (p5) => {
+      const stroke = 0;
+
+      // only on rubberband effect
+      if (isRubber) {
+        p5.clear();
+      }
+
+      p5.drawingContext.save();
+      // draw mask
+      p5.strokeWeight(stroke);
+      p5.stroke("white");
+      p5.fill(p5.color("rgba(255, 255, 255, 0)"));
+      const half = _size / 2;
+      if (resizeTo) {
+        polygon(half, half + 1, half - 1, 7, 3, 3, p5.PI / 2.8);
+      } else {
+        polygon(half, half + 7, half - stroke, 7, 20, 5, p5.PI / 2.8);
+      }
+      p5.drawingContext.clip();
+      // draw photo
+      const pos = {
+        x: -(imageSize.width * scale - size) / 2 + x,
+        y: -(imageSize.height * scale - size) / 2 + y,
+      };
+      if (resizeTo) {
+        const factor = resizeTo / size;
+        // console.log(x, factor, x * factor);
+        p5.image(
+          photoImage,
+          -(imageSize.width * scale * factor - resizeTo) / 2 + x * factor,
+          -(imageSize.height * scale * factor - resizeTo) / 2 + y * factor,
+          imageSize.width * scale * factor,
+          imageSize.height * scale * factor
+        );
+      } else {
+        p5.image(
+          photoImage,
+          pos.x,
+          pos.y,
+          imageSize.width * scale,
+          imageSize.height * scale
+        );
+      }
+      p5.drawingContext.restore();
+
+      p5.strokeWeight(resizeTo ? 1 : 3);
+      p5.stroke("white");
+      if (resizeTo) {
+        polygon(half, half + 1, half - 1, 7, 3, 3, p5.PI / 2.8);
+      } else {
+        polygon(half, half + 7, half - stroke, 7, 20, 5, p5.PI / 2.8);
+      }
+
+      /** A primitive function for rounded polygonal shape
+       * Polygon is circumscribed in a circle of radius 'size'
+       * Round corners radius is speci
+       * similar to circle(x,y,s) with an extra parameters
+       * @param x - x center of polygon
+       * @param y - y center of polygon
+       * @param size - size of polygon (radius of circumscribed circle)
+       * @param sides - number of polygon sides
+       * @param radius - radius of rounded corners
+       * @param res - angular resolution of each corner in points (default 5)
+       * @param rot - global rotation applied to shape (default 0)
+       * @author Gilles Gonon - http://gilles.gonon.free.fr
+       * @license GPL
+       */
+      function polygon(x, y, size, sides = 3, radius = 0, res = 5, rot = 0) {
+        // Polygon is drawned inside a circle
+        // Angle of 1 corner of polygon
+        let apoly = (sides > 2 ? (sides - 2) * p5.PI : p5.TWO_PI) / sides;
+        // Radius angle
+        let aradius = sides > 2 ? p5.PI - apoly : p5.PI;
+        // distance between vertex and radius center
+        let r = 2 * radius * p5.sin(p5.HALF_PI - 0.5 * apoly);
+        p5.push();
+        // debug log
+        // console.log('Polygon : sides '+sides+', apoly:'+degrees(apoly).toPrecision(4)+', aradius:'+degrees(aradius).toPrecision(4)+',distance from vertex for radius:'+rproj.toPrecision(4))
+
+        // Start drawing
+        p5.push();
+        p5.translate(x, y);
+        p5.rotate(rot);
+        p5.beginShape();
+        for (let a = 0; a < sides; a++) {
+          // Rotation for polygon vertex
+          let rot = (a * p5.TWO_PI) / sides;
+          if (radius) {
+            // Vertex coordinates
+            let cx = (size - r) * p5.cos(rot);
+            let cy = (size - r) * p5.sin(rot);
+            for (let i = 0; i < res; i++) {
+              let rotrad = rot + (i * aradius) / (res - 1) - 0.5 * aradius;
+              let px = radius * p5.cos(rotrad);
+              let py = radius * p5.sin(rotrad);
+              p5.vertex(cx + px, cy + py);
+              // circle(cx + px, cy + py, 0.15*i+2)
+            }
+            // circle(cx, cy, 0.15*a+2)
+          } else {
+            let dx = size * p5.cos(rot);
+            let dy = size * p5.sin(rot);
+            p5.vertex(dx, dy);
+            // circle(dx,dy, 0.15*a+2);
+          }
+        }
+        p5.endShape(p5.CLOSE);
+        p5.pop();
+      }
+    };
+
+    return <Sketch key={key} preload={preload} setup={setup} draw={draw} />;
+  }
+);
 
 function setMiniPreviewScale(size) {
   return {
@@ -238,9 +373,10 @@ function setMiniPreviewScale(size) {
 //   };
 // }
 
+const map = (value, x1, y1, x2, y2) =>
+  ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+
 function renderPreview({ x, y }) {
-  const map = (value, x1, y1, x2, y2) =>
-    ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
   // 100 - 0
   // -100 - 100
   return {
